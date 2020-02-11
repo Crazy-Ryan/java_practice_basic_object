3fe10400ec7d65bb557ca735dcd7ae956d56b19b
Ryan.Wang <ranwang1995@outlook.com>
2020-02-11 21:14:17 +0800

    [WangRan] feat: create student object - LiLi

--- 002_object/src/main/java/Application.java
+++ 002_object/src/main/java/Application.java
@@ -1,6 +1,12 @@
+import model.Student;
+
 public class Application {
 
     public static void main(String[] args) {
+        Student student = new Student("李莉", 20, "女");
+        System.out.println("姓名: " + student.getName());
+        System.out.println("年龄: " + student.getAge());
+        System.out.println("性别: " + student.getSex());
 
     }
 }
